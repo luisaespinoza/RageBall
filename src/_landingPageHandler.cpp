720aea355ed735bd999ff7dd79aabce7e20b2f40
luisaespinoza <luisaespinoza20562@gmail.com>
2025-12-08 15:17:40 -0800

    debugging start box and getting placement right

--- src/_landingPageHandler.cpp
+++ src/_landingPageHandler.cpp
@@ -44,27 +44,27 @@ int _landingPageHandler::winMsg(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPar
             int mx = LOWORD(lParam), my = HIWORD(lParam);
             int myInv = LandingPage->H - my;
 
-            // // --- DEBUG: measure button rect by 2 clicks ---
-            // static int clickStage = 0;
-            // static buttonPage debugRect;
+            // --- DEBUG: measure button rect by 2 clicks ---
+            static int clickStage = 0;
+            static buttonPage debugRect;
 
             if (clickStage == 0) {
-                // first click: top-left of desired button region
-                // debugRect.x = mx;
-                // debugRect.y = myInv;
-                // clickStage = 1;
-                // std::cout << "[CALIB] top-left = (" << mx << ", " << myInv << ")\n";
+                first click: top-left of desired button region
+                debugRect.x = mx;
+                debugRect.y = myInv;
+                clickStage = 1;
+                std::cout << "[CALIB] top-left = (" << mx << ", " << myInv << ")\n";
             } else if (clickStage == 1) {
-                // second click: bottom-right of desired button region
-                // debugRect.w = mx - debugRect.x;
-                // debugRect.h = myInv - debugRect.y;
-                // clickStage = 2;
-                // std::cout << "[CALIB] bottom-right = (" << mx << ", " << myInv << ")\n";
-                // std::cout << "[CALIB] btnEnter = { "
-                //           << debugRect.x << ", "
-                //           << debugRect.y << ", "
-                //           << debugRect.w << ", "
-                //           << debugRect.h << " };\n";
+                second click: bottom-right of desired button region
+                debugRect.w = mx - debugRect.x;
+                debugRect.h = myInv - debugRect.y;
+                clickStage = 2;
+                std::cout << "[CALIB] bottom-right = (" << mx << ", " << myInv << ")\n";
+                std::cout << "[CALIB] btnEnter = { "
+                          << debugRect.x << ", "
+                          << debugRect.y << ", "
+                          << debugRect.w << ", "
+                          << debugRect.h << " };\n";
             }
 
             // normal behavior:
