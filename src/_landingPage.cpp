210b6286f8ae984f588dfec8df5a149023680113
luisaespinoza <luisaespinoza20562@gmail.com>
2025-12-08 16:13:45 -0800

    debugging start box and getting placement right

--- src/_landingPage.cpp
+++ src/_landingPage.cpp
@@ -4,7 +4,7 @@ namespace {
     constexpr int BASE_H = 1080;
 
     // measured from TOP-LEFT in the art:
-    constexpr int BTN_X_TOP = 95;
+    constexpr int BTN_X_TOP = 105;
     constexpr int BTN_Y_TOP = 525;
     constexpr int BTN_W     = 482;
     constexpr int BTN_H     = 114;
